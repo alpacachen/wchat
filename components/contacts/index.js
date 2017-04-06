09ea59e6aac87de0883a7bb32725a7bf136853c7
liuzhongyang <liuzhongyang@huoqiu.cn>
2017-04-06 09:32:33 +0800

    ch

--- components/contacts/index.js
+++ components/contacts/index.js
@@ -6,6 +6,11 @@ var html = handlebars.compile(require('./index.html'));
 var func = function(){
     //footer跳转
     require("../common/ch.js")();
+    //首字母导航栏
+    $(".slidebar").on("tap","span",function(){
+    	// window.scrollTop($(".to"+$(this).attr("class")).scrollTop())
+    	console.log($("toB").height())
+    })
 };
 module.exports = function() {
     return {
